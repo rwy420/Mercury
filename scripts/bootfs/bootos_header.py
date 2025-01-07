e9b03adcdc1d868640b98ed418e2c05def7cba8f
rwy420 <.>
2025-01-07 17:47:30 +0100

    Minor BootFS change

--- scripts/bootfs/bootos_header.py
+++ scripts/bootfs/bootos_header.py
@@ -2,7 +2,7 @@
 
 end_address = 17 * 0x200
 
-boot_entry = "KERNEL+STAGE2\0".encode() + bytes.fromhex("000000000000000001") + bytes.fromhex("00000000") + bytes.fromhex("00004000") + bytes.fromhex("00");
+boot_entry = "KERNEL+STAGE2".encode() + bytes.fromhex("0000000000000000000001") + bytes.fromhex("00000000") + bytes.fromhex("00004000");
 
 with open("header.bin", "wb") as file:
     file.write(boot_entry);
