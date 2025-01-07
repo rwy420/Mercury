e7719500495499d2f3bd16a135cf8c6bbb49f3b1
rwy420 <.>
2025-01-07 17:54:11 +0100

    Script changes

--- scripts/bootfs/bootos_header.py
+++ scripts/bootfs/bootos_header.py
@@ -2,7 +2,7 @@
 
 end_address = 17 * 0x200
 
-boot_entry = "KERNEL+STAGE2".encode() + bytes.fromhex("0000000000000000000001") + bytes.fromhex("00000000") + bytes.fromhex("00004000");
+boot_entry = "KERNEL+STAGE2".encode() + bytes.fromhex("0000000000000000000001") + bytes.fromhex("0000000A") + bytes.fromhex("0000004A");
 
 with open("header.bin", "wb") as file:
     file.write(boot_entry);
