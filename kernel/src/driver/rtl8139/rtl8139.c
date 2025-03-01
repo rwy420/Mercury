c931891021277569fb4bb1f97c2746a3458c7a54
rwy420 <.>
2025-03-01 11:48:20 +0100

    Cleaned up

--- kernel/src/driver/rtl8139/rtl8139.c
+++ kernel/src/driver/rtl8139/rtl8139.c
@@ -1,5 +1,5 @@
-#define ROK (1<<0)
-#define TOK (1<<2)
+#define STATUS_ROK (1<<0)
+#define STATUS_TOK (1<<2)
 
 #define MAC_0_5_PORT(base) base
 #define MAC_5_6_PORT(base) base + 0x04
@@ -18,18 +18,6 @@
 #include <memory/common.h>
 #include <net/nic.h>
 
-enum RTL8139Registers {
-  RTL8139_REG_MAC0_5 = 0x00,
-  RTL8139_REG_MAC5_6 = 0x04,
-  RTL8139_REG_MAR0_7 = 0x08,
-  RTL8139_REG_RBSTART = 0x30,
-  RTL8139_REG_CMD = 0x37,
-  RTL8139_REG_IMR = 0x3C,
-  RTL8139_REG_ISR = 0x3E,
-
-  RTL8139_REG_POWERUP = 0x52
-};
-
 static EthernetInterface* interface;
 static uint32_t port_base;
 
@@ -47,10 +35,10 @@ static void rtl8139_handle_interrupt()
     
     uint16_t status = inw(port_base + 0x3e);
 
-    if(status & TOK) {
+    if(status & STATUS_TOK) {
         printf("Packet sent\n");
     }
-    if (status & ROK) {
+    if (status & STATUS_ROK) {
         printf("Received packet\n");
         rtl8139_receive();
     }
