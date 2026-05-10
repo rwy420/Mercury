556ec929c3adf8c915e03461e701d140e31efb10
rwy420 <.>
2026-05-10 12:59:17 +0200

    Filled out values of the FAT32Volume struct on fat32 initialization

---
+++ kernel/include/fs/fat32/fat32.h
@@ -0,0 +1,20 @@
+#ifndef __MERCURY__FS__FAT32__FAT32_H
+#define __MERCURY__FS__FAT32__FAT32_H
+
+#include <fs/disk.h>
+#include <fs/fat/bpb.h>
+
+typedef struct
+{
+	BPB bpb;
+	EBPB_FAT32 ebpb;
+	PartitionTableEntry partition;
+	uint32_t fat_start;
+	uint32_t data_start;
+	uint32_t root_cluster;
+	uint32_t sectors_per_cluster;
+} FAT32Volume;
+
+int fat32_init(BPB* bpb, EBPB_FAT32* ebpb, PartitionTableEntry* partition);
+
+#endif
