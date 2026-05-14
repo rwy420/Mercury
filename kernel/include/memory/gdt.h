3315cf873056d1e4087973edc870a169b5f1298d
rwy420 <.>
2026-05-14 19:24:29 +0200

    Now calculating memory segments

--- kernel/include/memory/gdt.h
+++ kernel/include/memory/gdt.h
@@ -3,11 +3,11 @@
 
 #include <common/types.h> 
 
-#define KERNEL_CODE_SEGMENT 0x08
-#define KERNEL_DATA_SEGMENT 0x10
-#define USER_CODE_SEGMENT 0x18
-#define USER_DATA_SEGMENT 0x20
-#define TSS_SEGMENT 0x28
+#define KERNEL_CODE_SEGMENT ((1 << 3) | 0)
+#define KERNEL_DATA_SEGMENT ((2 << 3) | 0)
+#define USER_CODE_SEGMENT ((3 << 3) | 0)
+#define USER_DATA_SEGMENT ((4 << 3) | 0)
+#define TSS_SEGMENT (5 << 3) | 0
 
 typedef struct
 {
--- kernel/src/memory/gdt.c
+++ kernel/src/memory/gdt.c
@@ -34,8 +34,8 @@ void segments_install_gdt()
 	gdt_set_entry(4, 0, 0xFFFFF, 0xF2, 0xCF);
 	gdt_set_entry(5, (uint32_t) &g_tss, sizeof(TSS), 0x89, 0x40);
 
-	g_tss.ss0 = 0x10;
-	g_tss.esp0 = 0xC0090000;
+	g_tss.ss0 = KERNEL_DATA_SEGMENT;
+	g_tss.esp0 = 0x90000;
 
 	segments_load_gdt(g_gdt);
 	segments_load_registers();
